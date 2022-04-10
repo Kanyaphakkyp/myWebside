f3564072419c6646ed5d1412ae7b4e5fc0af0e58
Kanyaphakkyp <100659257+Kanyaphakkyp@users.noreply.github.com>
2022-04-11 00:49:46 +0700

    Add files via upload

---
+++ ndvi/layers/layers.js
@@ -0,0 +1,30 @@
+var wms_layers = [];
+
+
+        var lyr_OSMStandard_0 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var lyr_ndvi_1 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "ndvi",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/ndvi_1.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [11181420.000000, 1514422.579620, 11239260.000000, 1541923.507265]
+                            })
+                        });
+
+lyr_OSMStandard_0.setVisible(true);lyr_ndvi_1.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_ndvi_1];
